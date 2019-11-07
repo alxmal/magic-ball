70971085d381e5f4a0a865e47e52ed126d945ce0
Alexandr Malafeev <alxmalafeev@gmail.com>
2019-11-07 11:28:27 +0300

    change lang on answers

--- src/js/common.js
+++ src/js/common.js
@@ -1,12 +1,12 @@
 function makeAdvice() {
-		var advice = ['Бесспорно', 'Предрешено', 'Никаких сомнений', 
-				'Определённо да', 'Можешь быть уверен в этом', 
-				'Мне кажется да', 'Вероятнее всего', 
-				'Хорошие перспективы', 'Хорошие перспективы', 
-				'Знаки говорят «да»', 'Да','Пока не ясно, попробуй снова',
-				'Спроси позже', 'Лучше не рассказывать', 'Сейчас нельзя предсказать',
-				'Сконцентрируйся и спроси опять', 'Даже не думай', 'Мой ответ нет',
-				'По моим данным нет', 'Перспективы не очень хорошие', 'Весьма сомнительно'];
+		var advice = ['As I see it, yes', 'Ask again later', 'Better not tell you now', 
+				'Cannot predict now', 'Concentrate and ask again', 
+				'Don\'t count on it', 'It is certain', 
+				'It is decidedly so', 'Most likely', 
+				'My reply is no', 'Да','My sources say no',
+				'Outlook not so good', 'Outlook good', 'Reply hazy, try again',
+				'Signs point to yes', 'Very doubtful', 'Without a doubt',
+				'Yes', 'Yes – definitely', 'You may rely on it'];
 
 		var randomRoll 		= Math.floor(Math.random() * advice.length);
 		var ballSays 		= advice[randomRoll];
